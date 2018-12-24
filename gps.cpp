070222fba0c99642082a26b8497fdaa8ac15b81a
Alvaro Prieto <source@alvaroprieto.com>
2018-12-24 13:20:24 -0800

    Updated gps class to use interrupts for uart rx

--- gps.cpp
+++ gps.cpp
@@ -1,27 +1,55 @@
 #include "gps.h"
 #include "mbed.h"
+#include <string.h>
+#include "fifo.h"
 
+#define FIFO_BUFF_SIZE  (2048)
+
+static uint8_t inBuff[FIFO_BUFF_SIZE];
 
 GPS::GPS(PinName tx_pin, PinName rx_pin)
     : uart(tx_pin, rx_pin) {
+
+    fifoInit(&rxFifo, FIFO_BUFF_SIZE, inBuff);
+
+    // Setup uart RX irq
+    uart.attach(callback(GPS::irq, this));
 }
 
+void GPS::irq(GPS *gps_obj) {
+    fifoPush(&gps_obj->rxFifo, gps_obj->uart.getc());
+}
+
+// Read NMEA line/sentence if available
 uint8_t * GPS::readline() {
-    uint16_t index = 0;
-    bool scanning = true;
+    uint16_t len = 0;
+    gps_linebuff[0] = 0;
 
-    while(scanning) {
-        gps_linebuff[index] = uart.getc();
+    // Find start of sentence and discard other characters
+    while (fifoSize(&rxFifo) && fifoPeek(&rxFifo, 0) != '$') {
+        fifoPop(&rxFifo);
+    }
 
-        if (gps_linebuff[index] == '\n') {
-            gps_linebuff[index] = 0;
-            scanning = false;
+    if (fifoSize(&rxFifo) && fifoPeek(&rxFifo, 0) == '$') {
+        // Find end of sentence
+        for(uint32_t byte = 0; byte < fifoSize(&rxFifo); byte++) {
+            if(fifoPeek(&rxFifo, byte) == '\n') {
+                len = byte + 1;
+                break;
+            }
         }
+    }
 
-        index++;
-
-        if (index >= (sizeof(gps_linebuff) - 1)) {
-            index = 0;
+    if(len < (sizeof(gps_linebuff) - 1)) {
+        uint16_t index = 0;
+        while(len--) {
+            gps_linebuff[index++] = fifoPop(&rxFifo);
+        }
+        gps_linebuff[index] = 0;
+    } else {
+        // Line too long, remove from fifo
+        while(len--) {
+            fifoPop(&rxFifo);
         }
     }
 
--- gps.h
+++ gps.h
@@ -2,15 +2,17 @@
 #define __GPS_H__
 
 #include "mbed.h"
+#include "fifo.h"
 
 class GPS {
 public:
     GPS(PinName tx_pin, PinName rx_pin);
     uint8_t * readline();
-
 private:
-    Serial uart;
+    static void irq(GPS *gah);
     uint8_t gps_linebuff[256];
+    fifo_t rxFifo;
+    RawSerial uart;
 };
 
 #endif
