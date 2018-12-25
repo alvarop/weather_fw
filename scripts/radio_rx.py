9e8cd8265cf98ff64aed58808a65cdf2eaa87195
Alvaro Prieto <source@alvaroprieto.com>
2018-12-24 17:54:19 -0800

    Update radio_rx.py to save to csv and sqlite.db

--- scripts/radio_rx.py
+++ scripts/radio_rx.py
@@ -1,18 +1,108 @@
 #!/usr/bin/env python
-'''
+"""
 Decode incoming packets from weather station
-'''
+"""
 
-import subprocess
-import time
+import argparse
+import os
 import struct
 import serial
 import sys
+import sqlite3
+import time
 from crc import crc16
+from datetime import datetime
 
 HEADER_LEN = 4
 CRC_LEN = 2
 START_BYTES = 0xaa55
+
+wx_keys = {
+    "te": "temperature",
+    "ws": "wind_speed",
+    "wd": "wind_dir",
+    "ra": "rain",
+    "hu": "humidity",
+    "li": "light",
+    "pr": "pressure",
+}
+
+data_columns = [
+    "timestamp",
+    "temperature",
+    "humidity",
+    "pressure",
+    "light",
+    "rain",
+    "wind_speed",
+    "wind_dir",
+]
+
+sql_insert = "INSERT INTO samples VALUES(NULL,{})".format(
+    ",".join(["?"] * len(data_columns))
+)
+
+
+def save_sqlite_data(data):
+    line = []
+    for key in data_columns:
+        if key == "timestamp":
+            line.append(data[key])
+        else:
+            line.append(int(data[key] * 1000))
+    cur.execute(sql_insert, line)
+
+    # Add retries in case the database is locked
+    # Rpi zero is slow and the ostur_viewer hogs it when a request comes in
+    retries = 5
+    while retries > 0:
+        try:
+            con.commit()
+            break
+        except sqlite3.OperationalError:
+            print("Unable to commit. Retrying {}".format(retries))
+            retries -= 1
+            continue
+
+
+def save_csv_data(data):
+    line = ""
+
+    for key in data_columns:
+        if key == "timestamp":
+            line += datetime.fromtimestamp(data["timestamp"]).strftime(
+                "%Y-%m-%d %H:%M:%S"
+            )
+        else:
+            line += str(data[key])
+
+        line += ","
+
+    csvfile.write(line + "\n")
+    csvfile.flush()
+
+
+def write_csv_header():
+    header = ""
+    for item in data_columns:
+        header += str(item) + ","
+    csvfile.write(header + "\n")
+
+
+def print_data(data):
+    line = ""
+
+    for key in data_columns:
+        if key == "timestamp":
+            line += datetime.fromtimestamp(data["timestamp"]).strftime(
+                "%Y-%m-%d %H:%M:%S"
+            )
+        else:
+            line += str(data[key])
+        line += ","
+
+    print(line)
+
 
 def check_crc(packet):
     packet_crc = struct.unpack("H", packet[-CRC_LEN:])[0]
@@ -20,9 +110,31 @@ def check_crc(packet):
     return packet_crc == computed_crc
 
 
+def process_wx_str(wx_str):
+    data = {}
+    data["timestamp"] = int(time.time())
+
+    for line in wx_str.split("\n"):
+        key, val = line.split(":")
+        if key in wx_keys:
+            data[wx_keys[key]] = float(val)
+        else:
+            data[key] = val
+
+    if args.csvfile:
+        save_csv_data(data)
+
+    if args.db:
+        save_sqlite_data(data)
+
+    print_data(data)
+
+
 def process_packet(packet):
     try:
-        print(packet[HEADER_LEN:-CRC_LEN].decode('utf-8').strip())
+        packet_str = packet[HEADER_LEN:-CRC_LEN].decode("utf-8").strip()
+        process_wx_str(packet_str)
+
     except UnicodeDecodeError:
         pass
 
@@ -60,8 +172,46 @@ def decode(buff):
                     return True
 
 
-stream = serial.Serial(sys.argv[1], baudrate=115200)
-stream.timeout = 0.01
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--baud_rate", default=115200, type=int, help="xbee baud rate")
+
+parser.add_argument("--port", required=True, help="xbee device to connect to")
+
+parser.add_argument("--csvfile", help="Output csvfile (csv format)")
+
+parser.add_argument("--db", help="Sqlite db csvfile")
+
+args = parser.parse_args()
+
+if args.csvfile and os.path.isfile(args.csvfile):
+    need_header = False
+else:
+    need_header = True
+
+if args.csvfile:
+    csvfile = open(args.csvfile, mode="a")
+
+    if need_header:
+        write_csv_header()
+
+if args.db:
+    con = None
+    con = sqlite3.connect(args.db)
+
+    if con is None:
+        raise IOError("Unable to open sqlite database")
+
+    cur = con.cursor()
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS "
+        + "samples(id INTEGER PRIMARY KEY,{} INTEGER)".format(
+            " INTEGER, ".join(data_columns)
+        )
+    )
+
+stream = serial.Serial(args.port, baudrate=args.baud_rate, timeout=0.01)
+stream.flushInput()
 
 buff = bytearray()
 
@@ -71,4 +221,3 @@ while True:
         buff.append(line[0])
         while decode(buff) is True:
             pass
-
